5dd52ff3218863f14f5e59da54b03be39af5b74d
MULX <mulxman@gmail.com>
2023-08-28 23:52:14 +1000

    Base components added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <BreedList msg="Breed List"/>
+  <BreedImages msg="Breed Images"/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import BreedList from './components/BreedList.vue';
+import BreedImages from './components/BreedImages.vue';
 
 @Options({
   components: {
     HelloWorld,
+    BreedList,
+    BreedImages
   },
 })
 export default class App extends Vue {}
