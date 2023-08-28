0bd864474992b89eccef97c7ac1c1b2e66f6e688
MULX <mulxman@gmail.com>
2023-08-28 23:39:09 +1000

    Assets added

---
+++ src/components/BreedImages.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <p>Show 10 images from the selected breed, if two breeds are selected show 10 images in total 5 from each breed.</p>
+        <p>Ability to remove images, once all images of a breed are removed that breed should be unselected in the list.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BreedImages',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
---
+++ src/components/BreedList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Selected Breeds</h1>
+    <ul>
+      <li v-for="breed in selectedBreeds" :key="breed">{{ breed }}</li>
+    </ul>
+    <button @click="updateSelectedBreeds(['Golden Retriever', 'Labrador'])">Select Breeds</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState({
+      selectedBreeds: (state) => state.selectedBreeds,
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      updateSelectedBreeds: 'setSelectedBreeds',
+    }),
+  },
+};
+</script>
